10ce6a54b6b2cdd6a5ee62d85a6d5690e91c0029
Gandhi Hernández <79735730+Yasser-Gandhi@users.noreply.github.com>
2022-09-28 15:52:44 -0500

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { BlogComponent } from './blog/blog.component';
+import { CursosComponent } from './cursos/cursos.component';
+import { ContactoComponent } from './contacto/contacto.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
-const routes: Routes = [];
+
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'cursos', component: CursosComponent},
+  {path:'contacto', component: ContactoComponent},
+  {path:'blog', component: BlogComponent},
+  {path:'**', component: NotFoundComponent},
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
